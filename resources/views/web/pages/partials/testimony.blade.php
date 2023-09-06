fa1c4306420fc722776f2786bedb8a0f4e77a08d
Balazs Ercsey <laze@laze.hu>
2023-09-06 11:10:40 +0200

    Fix testimony.

--- resources/views/web/pages/partials/testimony.blade.php
+++ resources/views/web/pages/partials/testimony.blade.php
@@ -20,3 +20,46 @@
         <a href="" class="defbtn white"><i class="icon-play"></i>view our showreel</a>
     </div>
 </div>
+
+<!-- our mission popup -->
+
+<div class="our-mission-popup">
+    <div class="our-mission-back">
+      <a href="" class="defbtn close-popup"><i class="icon-arrow-left"></i>back to home</a>
+    </div>
+    <div class="our-mission-content">
+      <div class="content-wrapper">
+        <h2>Our Mission Statement</h2>
+        <h3>The digital age is all about connectivity. We specialize in discovering and forging links between
+          technologies, people, clients, and businesses. Operating beyond the conventional, our strong focus revolves
+          around invigorating our clients' businesses and expanding their horizons. </h3>
+        <p>
+          <b>In 2002, as we set forth on this path.</b> The idea of digital—or, as we like to imagine it—interactive
+          communication, remained a distant and indistinct notion for most. Or, to put it in business terms, it was the
+          last row in the Excel spreadsheet. Yet, we firmly believed in its potential! We believed that this was the
+          right
+          direction:<br>
+          - conversations over clamor<br>
+          - precision over shot in the dark<br>
+          - content over advertisement<br>
+          - individuality over mass.
+        </p>
+
+        <p>The driving force behind these changes and opportunities for transformation was, of course, the <b>new and
+            progressive technology</b>. Grounded in this landscape, we evangelized, innovated, experimented,
+          occasionally
+          stumbled, but on the whole, we forged ahead into the unknown. Looking back, it is evident that it was a
+          revolution, and we stood at the forefront of the barricades.</p>
+
+        <p>And now, we have indeed reached our goal. With a modest contribution from us, the landscape of the
+          communication industry has undergone a complete transformation. What was once a dream has become a daily
+          practice, shaped by established—some by us—standards.</p>
+
+        <p>Our new task is to preserve the values we have achieved. To uphold the excellence, the quality, to resist
+          unwarranted deviations, and to ensure sustainability, long-term functionality, and the prerequisites of
+          actionable knowledge—both at the organizational and industry levels. This is our new mission, and it is why we
+          work tirelessly. </p>
+      </div>
+
+    </div>
+  </div>
