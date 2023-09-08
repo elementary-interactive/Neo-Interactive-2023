4808269c4ff9d880bde6774d10cd55984f532acc
Elementary Interactive\Balázs Ercsey <laze@laze.hu>
2023-09-08 11:27:50 +0200

    Fix testimony to be dynamic.

--- app/Nova/Flexible/Layouts/TestimonyLayout.php
+++ app/Nova/Flexible/Layouts/TestimonyLayout.php
@@ -2,10 +2,12 @@
 
 namespace App\Nova\Flexible\Layouts;
 
+use Laravel\Nova\Fields\Heading;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Markdown;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Trix;
 use Whitecube\NovaFlexibleContent\Layouts\Layout;
 
 class TestimonyLayout extends Layout
@@ -33,6 +35,17 @@ class TestimonyLayout extends Layout
     public function fields()
     {
         return [
+            Heading::make('Weboldal blokk'),
+            Text::make('Cím', 'main_title'),
+            Text::make('Alcím', 'main_subtitle'),
+            Text::make('1. gomb szövege', 'main_cta1_title'),
+            Text::make('2. gomb szövege', 'main_cta2_title'),
+            Text::make('Showreel gomb szövege', 'main_cta-showreel_title'),
+            Heading::make('Belső popup'),
+            Text::make('Vissza gomb szövege', 'popup_cta-back_title'),
+            Text::make('Cím', 'popup_title'),
+            Text::make('Alcím', 'popup_subtitle'),
+            Trix::make('Tartalom', 'popup_content'),
         ];
     }
 }
--- resources/views/web/pages/partials/testimony.blade.php
+++ resources/views/web/pages/partials/testimony.blade.php
@@ -3,21 +3,20 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <h2>We are neo<span class="yellow">.</span><br>
-                        A full-service communication
-                        and media agency with a digital focus.</h2>
+                    <h2>{{ $attributes['main_title'] }}<span class="yellow">.</span><br>
+                        {{ $attributes['main_subtitle'] }}</h2>
                     <div class="testimony-btn">
-                        <a href="#actionable-knowledge" class="defbtn"><i class="icon-arrow-right"></i>Actionable knowledge</a>
+                        <a href="#actionable-knowledge" class="defbtn"><i class="icon-arrow-right"></i>{{ $attributes['main_cta1_title'] }}</a>
                     </div>
                     <div>
-                        <a href="" class="defbtn open-popup"><i class="icon-arrow-right"></i>our mission</a>
+                        <a href="" class="defbtn open-popup"><i class="icon-arrow-right"></i>{{ $attributes['main_cta2_title'] }}</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="showreel" style="background-image: url('{{ Vite::asset('resources/images/main/showreel.png') }}')">
-        <a href="" class="defbtn white"><i class="icon-play"></i>view our showreel</a>
+        <a href="" class="defbtn white"><i class="icon-play"></i>{{ $attributes['main_cta-showreel_title'] }}</a>
     </div>
 </div>
 
@@ -25,40 +24,13 @@
 
 <div class="our-mission-popup">
     <div class="our-mission-back">
-      <a href="" class="defbtn close-popup"><i class="icon-arrow-left"></i>back to home</a>
+      <a href="" class="defbtn close-popup"><i class="icon-arrow-left"></i>{{ $attributes['popup_cta-back_title'] }}</a>
     </div>
     <div class="our-mission-content">
       <div class="content-wrapper">
-        <h2>Our Mission Statement</h2>
-        <h3>The digital age is all about connectivity. We specialize in discovering and forging links between
-          technologies, people, clients, and businesses. Operating beyond the conventional, our strong focus revolves
-          around invigorating our clients' businesses and expanding their horizons. </h3>
-        <p>
-          <b>In 2002, as we set forth on this path.</b> The idea of digital—or, as we like to imagine it—interactive
-          communication, remained a distant and indistinct notion for most. Or, to put it in business terms, it was the
-          last row in the Excel spreadsheet. Yet, we firmly believed in its potential! We believed that this was the
-          right
-          direction:<br>
-          - conversations over clamor<br>
-          - precision over shot in the dark<br>
-          - content over advertisement<br>
-          - individuality over mass.
-        </p>
-
-        <p>The driving force behind these changes and opportunities for transformation was, of course, the <b>new and
-            progressive technology</b>. Grounded in this landscape, we evangelized, innovated, experimented,
-          occasionally
-          stumbled, but on the whole, we forged ahead into the unknown. Looking back, it is evident that it was a
-          revolution, and we stood at the forefront of the barricades.</p>
-
-        <p>And now, we have indeed reached our goal. With a modest contribution from us, the landscape of the
-          communication industry has undergone a complete transformation. What was once a dream has become a daily
-          practice, shaped by established—some by us—standards.</p>
-
-        <p>Our new task is to preserve the values we have achieved. To uphold the excellence, the quality, to resist
-          unwarranted deviations, and to ensure sustainability, long-term functionality, and the prerequisites of
-          actionable knowledge—both at the organizational and industry levels. This is our new mission, and it is why we
-          work tirelessly. </p>
+        <h2>{{ $attributes['popup_title'] }}</h2>
+        <h3>{{ $attributes['popup_subtitle'] }}</h3>
+        {!! attributes['popup_content'] !!}
       </div>
 
     </div>
