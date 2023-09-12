fa3cfaa3e4cf1f4f9f73763d836f878cd7177b9d
Elementary Interactive\Balázs Ercsey <laze@laze.hu>
2023-09-12 13:01:01 +0200

    Content updates.

---
+++ resources/views/web/pages/partials/en_index.blade.php
@@ -0,0 +1,38 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <h1>
+                Neo aims <br class="d-xl-none">to propel<br>
+                your business <br class="d-xl-none">forward<span class="yellow">.</span>
+                <div class="intro-anim">
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                    <i></i>
+                </div>
+            </h1>
+            <p class="subline">
+                Established in 2002.<br>
+                Present in 27 countries worldwide.
+            </p>
+            <a href="#we-are-neo" class="defbtn"><i class="icon-arrow-right"></i>How we can help you</a>
+        </div>
+    </div>
+</div>
+
+<div class="scroll-down">
+    <p><img src="{{ Vite::asset('resources/images/scroll-mouse.svg') }}" alt="SCROLL to explore"> SCROLL to explore</p>
+</div>
