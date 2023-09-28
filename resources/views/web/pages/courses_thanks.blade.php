6a3c59d134d746ba317ab27e65c207e27fe3836b
Balazs Ercsey <laze@laze.hu>
2023-09-28 15:43:14 +0200

    Fix course thanks.

--- resources/views/web/pages/courses_thanks.blade.php
+++ resources/views/web/pages/courses_thanks.blade.php
@@ -7,10 +7,9 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <h2>Állásajánlatod megérkezett hozzánk:</h2>
                     <div class="row def-b-margin">
                         <div class="col-12 col-xl-9">
-                            <h1 class="mb-0">Köszönjük.</h1>
+                            <h1 class="mb-0">Sikeres regisztráció!</h1>
                         </div>
                     </div>
 
@@ -20,37 +19,12 @@
 
                         <div class="col-12 col-xl-6">
                             <div class="carrer-registration-desc basic-text-container">
-                                Állásajánlatod fölött most álmatlan éjszakák tömkelegét töltjük majd el.
+                                Következő kurzusunk témájáról és időpontjáról üzenetben értesítünk. 
                             </div>
                         </div>
 
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- showreel -->
-
-    <div class="main-testimony-container on-career">
-        <div class="testimony-content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12">
-                        <h2>We are neo<span class="yellow">.</span><br>
-                            A full-service communication
-                            and media agency with a digital focus<span class="yellow">.</span></h2>
-                        <div class="testimony-btn">
-                            <a href="" class="defbtn"><i class="icon-arrow-right"></i>DISCOVER THE NEO METHOD</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="showreel" style="background-image: url('images/main/showreel.png')">
-            <a href="" class="defbtn white showreel-btn"><i class="icon-play"></i>{{ $attributes['main_cta-showreel_title'] }}</a>
-            <div class="embed-youtube">
-            <iframe width="560" height="315" class="video-iframe" src="https://www.youtube.com/embed/2kPJU06DWLA?si=3WKzv3dj8Ip2G5yt" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
     </div>
