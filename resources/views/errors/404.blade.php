2c8de34f735aedefd6aab6639477045dc6abe5cb
Balazs Ercsey <laze@laze.hu>
2023-09-28 14:51:01 +0200

    Change texts.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -41,7 +41,7 @@
             <div class="row">
                 <div class="col-12">
                     <h1>404<span class="yellow">.</span></h1>
-                    <h3>{{ __("We couldn't find the content you were looking for. But we're always happy to find new ones for you!") }}</h3>
+                    <h3>{{ __("We couldn't find the content you were looking for.") }}</h3>
                     <a href="/" class="defbtn"><i class="icon-arrow-left"></i> {{ __("Back to main page") }}</a>
                 </div>
             </div>
