a9613578b4fe6b1c30ea659d05231829d20f1406
Elementary Interactive\Balázs Ercsey <laze@laze.hu>
2023-10-02 15:46:07 +0200

    Add Google codes.

--- resources/views/web/layouts/head/head.blade.php
+++ resources/views/web/layouts/head/head.blade.php
@@ -41,13 +41,15 @@
   'resources/js/splide.min.js',
   
 ]) --}}
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-NDXP56H');</script>
+<!-- End Google Tag Manager -->
 
 {{--
-@if (config('app.env') == 'production' && config('google.gtm-code'))
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','{{ config('google.gtm-code') }}');</script>
-    <!-- End Google Tag Manager -->
-@endif
 
 @if (config('app.env') == 'production' && config('facebook.pixel-id'))
 <!-- Meta Pixel Code --> <script> !function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '{{ config('facebook.pixel-id') }}'); fbq('track', 'PageView'); </script> <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id={{ config('facebook.pixel-id') }}&ev=PageView&noscript=1" /></noscript> <!-- End Meta Pixel Code -->
